3721ef2a7a961394ee2353cdc13c25cfa922cb4b
DoneForYou-LP <goaldoit81@gmail.com>
2026-03-09 04:29:42 +0700

    Implement Rupiah currency formatting function
    Add function to format input as Rupiah currency.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function formatRupiah(input){
+
+let angka = input.value.replace(/[^,\d]/g, "").toString();
+let split = angka.split(",");
+let sisa = split[0].length % 3;
+let rupiah = split[0].substr(0, sisa);
+let ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+if(ribuan){
+let separator = sisa ? "." : "";
+rupiah += separator + ribuan.join(".");
+}
+
+input.value = rupiah;
+
+}
 function kirimWA(){
 
 var nama = document.getElementById("nama").value;
